d9b498476fb8ff83a1957b83c865f66687853667
hersch18 <harshbhaksaar18@gmail.com>
2025-10-14 21:52:41 +0530

    Playwright Framework

---
+++ playwright.config.js
@@ -0,0 +1,32 @@
+// @ts-check
+const { defineConfig, devices } = require('@playwright/test');
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+const config = ({
+  testDir: './tests',
+  testMatch: ['**/*.specs.js'],
+  timeout: 40000,
+  expect :{
+
+    timeout : 40000
+  },
+  reporter : 'html',
+  use: {
+   
+    browserName : 'chromium',
+    headless : false
+  },
+
+  
+});
+
+module.exports = config
+
---
+++ tests/UiBasics.specs.js
@@ -0,0 +1,27 @@
+const { test, expect } = require('@playwright/test');
+
+test.only('First Playwright test', async ({ browser }) => {   // Anonymous function with fat arrow
+
+    //chrome - plugins / cookies
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    console.log(await page.title());
+    await expect(page).toHaveTitle("LoginPage Practise | Rahul Shetty Academy")
+    await page.locator('#username').fill("Harsh");  // Type is deprecated so we are not using it
+    await page.locator("[type = 'password']").fill("Learning");
+    await page.locator("#signInBtn").click();
+
+
+
+});
+
+test('Page Playwright test', async ({ page }) => {   // Anonymous function
+
+    // //chrome - plugins / cookies
+    // const context = await browser.newContext();
+    // const page = await context.newPage();
+    await page.goto("https://google.com");
+    console.log(await page.title());
+    await expect(page).toHaveTitle("Google")
+});
